1a5363ce1e3a05dec3432094c1a2b49d7c27dce2
Vineet <koppalkarvineet@gmail.com>
2020-03-22 15:34:34 -0400

    Percentage-closer filtering for shadows

--- A2_40029699/Assets/Shaders/textureShader.fs
+++ A2_40029699/Assets/Shaders/textureShader.fs
@@ -32,8 +32,24 @@ float getShadowAmount(vec4 fragPosLightSpace)
     float bias = max(0.05 * (1.0 - dot(normal, lightDir)), 0.005);
 
     // Check whether current frag pos is in shadow
-    float shadow = currentDepth - bias > closestDepth ? 1.0 : 0.0;
+    // Percentage-closer filtering (PCF)
+    float shadow = 0.0;
+    vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
+    for (int x = -1; x <= 1; ++x)
+    {
+        for (int y = -1; y <= 1; ++y)
+        {
+            float  pcfDepth = texture(shadowMap, projCoords.xy + vec2(x, y) * texelSize).r;
+            shadow += currentDepth - bias > pcfDepth ? 1.0 : 0.0;
+		}
+	}
+    shadow /= 9.0;
 
+    // Set shadow to 0 when outside the far_plane of light's frustum
+    if (projCoords.z > 1.0)
+    {
+        shadow = 0.0;
+	}
     return shadow;
 }
 
